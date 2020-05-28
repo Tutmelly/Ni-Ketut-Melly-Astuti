ff5eae5daec29c751dd7c3c255c27d4fbe67b6dc
Tutmelly <tutmelly117@gmail.com>
2020-05-28 17:39:36 +0800

    Kolaborasi react dan electron
    Menginstal setup, membuat program react & electron

---
+++ Tugas 2_React & Electron/Part 1 & 2/src/components/appointment/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class List extends Component {
+    render(){
+        return(
+            <div>
+                <h4>Ini List MELLY</h4>
+            </div>
+        )
+    }
+}
+
+export default List
+
